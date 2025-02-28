918d4eb2ecc89f151b0b5dcea4470084f6f04d60
SmitSatasiya <satasiyasmit@gmail.com>
2025-02-28 11:24:22 +0530

    user routes add

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,1 +1,11 @@
 const userModel = require('../model/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
+
+module.exports.registerUser = async (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+}
